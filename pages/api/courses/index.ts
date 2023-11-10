ce9d399c11a463dd358aebf9ca788fd336cc5ac1
Amir Ali <amiraliazimloo123@gmail.com>
2023-11-10 19:24:00 +0330

    api for create course completed

---
+++ pages/api/courses/index.ts
@@ -0,0 +1,29 @@
+import coursesModel from "@/models/course";
+import connectToDB from "@/utils/db";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  connectToDB();
+
+  if (req.method == "POST") {
+    try {
+      const { title } = req.body;
+
+      if (!title.trim() || title.length < 8) {
+        return res.status(422).json({ message: "title is not valid" });
+      }
+
+      await coursesModel.create({ title });
+      
+      return res.status(201).json({ message: "Course created successfully" });
+
+      
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ message: "Unkonown Internal Server Error" });
+    }
+  }
+};
+
+export default handler;
