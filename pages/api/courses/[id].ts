b4468cfe3a7d10759ff417cd4b0d471611210940
Amir Ali <amiraliazimloo123@gmail.com>
2023-11-10 21:11:51 +0330

    delete api completed

--- pages/api/courses/[id].ts
+++ pages/api/courses/[id].ts
@@ -24,6 +24,27 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
         .status(500)
         .json({ message: "Unkonown Internal Server Error" });
     }
+  }else  if (req.method == "PUT") {
+    try {
+      const { id } = req.query;
+      const { title } = req.body;
+
+      if(isValidObjectId(id)){
+        await coursesModel.findOneAndUpdate({ _id:id },{
+          title
+        })
+      
+        return res.status(200).json({ message: "Course Updated successfully" });
+      }else{
+        return res.status(422).json({ message: "Course ID is not valid" });
+      }
+
+      
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ message: "Unkonown Internal Server Error" });
+    }
   }
 };
 
