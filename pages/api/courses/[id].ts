33e023557d37c60133e4cf8f0db6505b502dae57
Amir Ali <amiraliazimloo123@gmail.com>
2023-11-10 21:04:25 +0330

    delete course completed

---
+++ pages/api/courses/[id].ts
@@ -0,0 +1,30 @@
+import coursesModel from "@/models/course";
+import connectToDB from "@/utils/db";
+import  { isValidObjectId } from "mongoose";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  connectToDB();
+
+  if (req.method == "DELETE") {
+    try {
+      const { id } = req.query;
+
+      if(isValidObjectId(id)){
+        await coursesModel.findOneAndDelete({_id:id})
+      
+        return res.status(200).json({ message: "Course created successfully" });
+      }else{
+        return res.status(422).json({ message: "Course ID is not valid" });
+      }
+
+      
+    } catch (error) {
+      return res
+        .status(500)
+        .json({ message: "Unkonown Internal Server Error" });
+    }
+  }
+};
+
+export default handler;
